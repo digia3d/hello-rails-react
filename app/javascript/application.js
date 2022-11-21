b888f6f9e037b3620b66604fbc92a3933a4bbba8
Aleksandra Ujvari <ujvari65@gmail.com>
2022-11-21 21:30:34 +0100

    Generate root controller and configure routes,database and app.js and views root

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,21 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import React from "react"
+import ReactDOM from "react-dom"
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+
+
